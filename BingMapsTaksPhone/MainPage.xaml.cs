dbc8f791af13b8c64ace749851cdc8130dd0aafb
Depechie <glenn.versweyveld@gmail.com>
2013-10-20 19:56:16 +0200

    Cimbalino added

--- BingMapsTaksPhone/MainPage.xaml.cs
+++ BingMapsTaksPhone/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Device.Location;
 using System.Windows;
+using Cimbalino.Phone.Toolkit.Services;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
 
@@ -7,6 +8,8 @@ namespace BingMapsTaksPhone
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        GeoCoordinate _location = new GeoCoordinate(51.40205, 4.46802);
+
         // Constructor
         public MainPage()
         {
@@ -15,13 +18,17 @@ namespace BingMapsTaksPhone
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GeoCoordinate location = new GeoCoordinate(51.40205, 4.46802);
-
             BingMapsTask task = new BingMapsTask();
-            task.Center = location;
-            task.SearchTerm = location.ToString();
+            task.Center = _location;
+            task.SearchTerm = _location.ToString();
             task.ZoomLevel = 9;
             task.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MapsService mapsService = new MapsService();
+            mapsService.Show(_location, _location.ToString(), 9);
         }
     }
 }
